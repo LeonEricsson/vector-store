264e70a257c4658baeeed0e208be215c275714ef
Leon Ericsson <leon.ericsson@icloud.com>
2024-06-28 15:20:38 +0200

    added exe time to test and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # vector-store
-a minimal vector storage in numpy
+a minimal vector storage in numpy.
+
+build your index of documents and query it using cosine, euclidean or manhattan similarity. 
+
+run
+
+```
+python3 test.py
+```
+
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+import timeit
 from sentence_transformers import SentenceTransformer
 from documents import document
 from storage import VectorStorage
+
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2')
 
@@ -13,8 +15,16 @@ vs = VectorStorage(embedder=model)
 vs.index(docs)
 
 query = "What did emma do in this story?"
-top_k_indices, scores = vs.search_top_k([query], k=3)
 
+run_search = lambda query: vs.search_top_k([query], k=3)
+
+runs = 100
+time = timeit.timeit(lambda: run_search(query), number=runs)
+
+top_k_indices, scores = run_search(query)
+
+print(f"Execution time {(time/runs) * 1000:.4f} ms")
+print()
 print(f"The most similar documents to the query '{query}' is:")
 print()
 print(f"{docs[top_k_indices[0][0]]}")

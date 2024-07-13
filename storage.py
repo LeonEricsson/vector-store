8a0e3797c89432edd0aac9d08feb891418e3a152
Leon Ericsson <leon.ericsson@icloud.com>
2024-07-13 15:17:49 +0200

    added option to not save embedding model as part of a pickle.dump().
    to complement, there is now a set_embedder() function

--- storage.py
+++ storage.py
@@ -3,29 +3,44 @@ from enum import Enum
 
 from typing import List
 
+
 class SimilarityMetric(Enum):
     COSINE = 0
     EUCLIDEAN = 1
     MANHATTAN = 2
+
 
 def cosine_similarity(x, y):
     x_norm = x / np.linalg.norm(x, axis=1, keepdims=True)
     y_norm = y / np.linalg.norm(y, axis=1, keepdims=True)
     return x_norm @ y_norm.T
 
+
 def euclidean_similarity(x, y):
     return -np.sqrt(np.sum((x[:, np.newaxis] - y) ** 2, axis=-1))
+
 
 def manhattan_similarity(x, y):
     return -np.sum(np.abs(x[:, np.newaxis] - y), axis=-1)
 
+
 sims = [cosine_similarity, euclidean_similarity, manhattan_similarity]
 
+
 class VectorStorage:
-    def __init__(self, embedder, similarity = SimilarityMetric.COSINE, query_prefix = ""):
-        self.embedder = embedder
-        self.similarity = sims[similarity.value]
-        self.query_prefix = query_prefix
+    def __init__(
+        self,
+        embedder,
+        similarity=SimilarityMetric.COSINE,
+        query_prefix="",
+        save_embedder=False,
+    ):
+        self._embedder = embedder
+        self._similarity_fn = sims[similarity.value]
+        self._query_prefix = query_prefix
+        self._index = np.zeros((0, embedder.truncate_dim))
+        self._save_embedder = save_embedder
+
 
     def index(self, docs: List[str]):
         """
@@ -33,7 +48,7 @@ class VectorStorage:
         """
         new_embeddings = self._embedder.encode(docs)
         self._index = np.concatenate([self._index, new_embeddings])
-    
+
     def search_top_k(self, queries: List[str], k: int = 10):
         """Batched search for top k similar entries in index."""
         queries = [self._query_prefix + query for query in queries]
@@ -48,3 +63,20 @@ class VectorStorage:
 
         row_indices = np.arange(similarities.shape[0])[:, np.newaxis]
         return top_k_indices, similarities[row_indices, top_k_indices]
+
+    def __getstate__(self):
+        """Custom method to define what gets pickled."""
+        state = self.__dict__.copy()
+        if not self._save_embedder:
+            del state["_embedder"]
+        return state
+
+    def __setstate__(self, state):
+        """Custom method to define how the object is reconstructed when unpickled."""
+        self.__dict__.update(state)
+        if "_save_embedder" not in state or not state["_save_embedder"]:
+            self._embedder = None
+
+    def set_embedder(self, embedder):
+        """Method to set the embedder after unpickling if it wasn't saved."""
+        self._embedder = embedder
